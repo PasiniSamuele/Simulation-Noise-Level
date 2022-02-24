050cae010c3f275d7ec9b83184accabeec89bf8d
Giorgio Piazza <gio.piazza97@gmail.com>
2022-02-24 18:51:04 +0100

    Fix noise source

--- contiki-ng/tools/cooja/java/org/contikios/cooja/motes/NoiseSourceMoteType.java
+++ contiki-ng/tools/cooja/java/org/contikios/cooja/motes/NoiseSourceMoteType.java
@@ -91,8 +91,8 @@ public class NoiseSourceMoteType extends AbstractApplicationMoteType {
     private final RadioPacket radioPacket = new COOJARadioPacket(new byte[] {
         0x01, 0x02, 0x03, 0x04, 0x05
     });
-    private final static long DELAY = Simulation.MILLISECOND/5;
-    private final static long DURATION = 10*Simulation.MILLISECOND;
+    private final static long DELAY = 200 * Simulation.MILLISECOND;
+    private final static long DURATION = 10 * Simulation.MILLISECOND;
     
     public NoiseSourceMote() {
       super();
