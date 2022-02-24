3f6e01626dbd85ef5df427b26309707ff78e0d8e
Samuele Negrini <samuele.negrini@mail.polimi.it>
2022-02-24 16:03:34 +0100

    Fix MQTT

--- contiki-ng/code/noise-sensor/mqtt-util.c
+++ contiki-ng/code/noise-sensor/mqtt-util.c
@@ -1,124 +1,4 @@
-/*---------------------------------------------------------------------------*/
-/** 
- *
- * Demonstrates MQTT functionality using a local Mosquitto borker. 
- * Published messages include a fake temperature reading.
- * @{
- *
- * \file
- * An MQTT example 
- */
-/*---------------------------------------------------------------------------*/
-#include "contiki.h"
-#include "mqtt.h"
-#include "rpl.h"
-#include "net/ipv6/uip.h"
-#include "net/ipv6/sicslowpan.h"
-#include "sys/etimer.h"
-#include "sys/ctimer.h"
-
-#include "sys/log.h"
-#define LOG_MODULE "MQTT-UTIL"
-#define LOG_LEVEL LOG_LEVEL_INFO
-
-#include <string.h>
-/*---------------------------------------------------------------------------*/
-/*
- * Publish to a local MQTT broker (e.g. mosquitto) running on
- * the node that hosts your border router
- */
-static const char *broker_ip = MQTT_BROKER_IP_ADDR;
-#define DEFAULT_ORG_ID              "mqtt-util"
-/*---------------------------------------------------------------------------*/
-/*
- * A timeout used when waiting for something to happen (e.g. to connect or to
- * disconnect)
- */
-#define STATE_MACHINE_PERIODIC     (CLOCK_SECOND >> 1)
-/*---------------------------------------------------------------------------*/
-/* Connections and reconnections */
-#define RETRY_FOREVER              0xFF
-#define RECONNECT_INTERVAL         (CLOCK_SECOND * 2)
-/*---------------------------------------------------------------------------*/
-/*
- * Number of times to try reconnecting to the broker.
- * Can be a limited number (e.g. 3, 10 etc) or can be set to RETRY_FOREVER
- */
-#define RECONNECT_ATTEMPTS         RETRY_FOREVER
-#define CONNECTION_STABLE_TIME     (CLOCK_SECOND * 5)
-static struct timer connection_life;
-static uint8_t connect_attempt;
-/*---------------------------------------------------------------------------*/
-/* Various states */
-static uint8_t state;
-#define STATE_INIT            0
-#define STATE_REGISTERED      1
-#define STATE_CONNECTING      2
-#define STATE_CONNECTED       3
-#define STATE_PUBLISHING      4
-#define STATE_DISCONNECTED    5
-#define STATE_NEWCONFIG       6
-#define STATE_CONFIG_ERROR 0xFE
-#define STATE_ERROR        0xFF
-/*---------------------------------------------------------------------------*/
-#define CONFIG_ORG_ID_LEN        32
-#define CONFIG_TYPE_ID_LEN       32
-#define CONFIG_AUTH_TOKEN_LEN    32
-#define CONFIG_CMD_TYPE_LEN       8
-#define CONFIG_IP_ADDR_STR_LEN   64
-/*---------------------------------------------------------------------------*/
-/* A timeout used when waiting to connect to a network */
-#define NET_CONNECT_PERIODIC        (CLOCK_SECOND >> 2)
-/*---------------------------------------------------------------------------*/
-/* Default configuration values */
-#define DEFAULT_TYPE_ID             "native"
-#define DEFAULT_AUTH_TOKEN          "AUTHZ"
-#define DEFAULT_SUBSCRIBE_CMD_TYPE  "+"
-#define DEFAULT_BROKER_PORT         1883
-#define DEFAULT_PUBLISH_INTERVAL    (60 * CLOCK_SECOND)
-#define DEFAULT_KEEP_ALIVE_TIMER    60
-/*---------------------------------------------------------------------------*/
-/**
- * \brief Data structure declaration for the MQTT client configuration
- */
-typedef struct mqtt_client_config {
-  char org_id[CONFIG_ORG_ID_LEN];
-  char type_id[CONFIG_TYPE_ID_LEN];
-  char auth_token[CONFIG_AUTH_TOKEN_LEN];
-  char broker_ip[CONFIG_IP_ADDR_STR_LEN];
-  char cmd_type[CONFIG_CMD_TYPE_LEN];
-  clock_time_t pub_interval;
-  uint16_t broker_port;
-} mqtt_client_config_t;
-/*---------------------------------------------------------------------------*/
-/* Maximum TCP segment size for outgoing segments of our socket */
-#define MAX_TCP_SEGMENT_SIZE    32
-/*---------------------------------------------------------------------------*/
-/*
- * Buffers for Client ID and Topic.
- * Make sure they are large enough to hold the entire respective string
- *
- * We also need space for the null termination
- */
-#define BUFFER_SIZE 64
-static char client_id[BUFFER_SIZE];
-static char pub_topic[BUFFER_SIZE];
-/*---------------------------------------------------------------------------*/
-/*
- * The main MQTT buffers.
- * We will need to increase if we start publishing more data.
- */
-#define APP_BUFFER_SIZE 512
-static struct mqtt_connection conn;
-static char app_buffer[APP_BUFFER_SIZE];
-/*---------------------------------------------------------------------------*/
-static struct mqtt_message *msg_ptr = 0;
-static struct ctimer ct;
-static char *buf_ptr;
-static uint16_t seq_nr_value = 0;
-/*---------------------------------------------------------------------------*/
-static mqtt_client_config_t conf;
-/*---------------------------------------------------------------------------*/
+#include "mqtt-util.h"
 
 int
 ipaddr_sprintf(char *buf, uint8_t buf_len, const uip_ipaddr_t *addr)
@@ -233,7 +113,7 @@ construct_client_id(void)
 }
 /*---------------------------------------------------------------------------*/
 static void
-update_config(etimer *mqtt_timer)
+update_config(struct etimer *mqtt_timer)
 {
   if(construct_client_id() == 0) {
     /* Fatal error. Client ID larger than the buffer */
@@ -272,7 +152,7 @@ update_config(etimer *mqtt_timer)
 }
 /*---------------------------------------------------------------------------*/
 static void
-init_config()
+init_config(void)
 {
   /* Populate configuration with default values */
   memset(&conf, 0, sizeof(mqtt_client_config_t));
@@ -354,7 +234,7 @@ connect_to_broker(void)
 }
 /*---------------------------------------------------------------------------*/
 static void
-state_machine(etimer *mqtt_timer)
+mqtt_state_machine(struct etimer *mqtt_timer)
 {
   switch(state) {
   case STATE_INIT:
